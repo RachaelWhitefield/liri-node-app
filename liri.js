f2d48dc5891306fd3a850f13066fb10e7e1084fb
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-06-26 12:23:51 -0600

    got movie-this and concert-this to work

--- liri.js
+++ liri.js
@@ -7,30 +7,48 @@ var spotify = new Spotify(keys.spotify);
 // 1557ce71 OMDB API KEY
 
 var action = process.argv[2].toLowerCase();
-var searchTerm = process.argv.slice(3);
+var searchMovie = process.argv.slice(3).join("+");
+var searchConcert = process.argv.slice(3).join("%20");
+var display = process.argv.slice(3).join(" ");
 
-function spotifySearch() {
-    console.log("This is the spotify search");
-}
+// function spotifySearch() {
+//     axios.get("")
+//     .then(function(response) {
+//         console.log(response);
+//     })
+//     console.log("This is the spotify search");
+// };
 
 function omdbSearch() {
-    axios.get("http://www.omdbapi.com/?apikey=1557ce71&t=" + searchTerm)
+    axios.get("http://www.omdbapi.com/?apikey=1557ce71&t=" + searchMovie)
   .then(function (response) {
-    // handle success
-    console.log(response);
+    console.log(`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB Rating: ${response.data.imdbRating}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}`);
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
   })
     console.log("This is the OMDB search");
 }
+var searchMovie = process.argv.slice(3).join("+");
 
 function concertSearch() {
-    console.log("This is the concert search");
-}
+    console.log(`\n\nUpcoming concerts for ${display}\n\n`);
+    axios.get("https://rest.bandsintown.com/artists/" + searchConcert + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+            console.log(`Venue: ${response.data[i].venue.name}\nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}\nDate: ${response.data[i].datetime}\n\n`);
+        }
+    });
+};
+
 
 function doWhatItSays() {
+    fs.appendFile("random.txt", function(error, data) {
+        if (error) {
+            return console.log(error);
+        } else
+            console.log("Backstreet Boys for LIFE");
+    })
     console.log("This is Do what it says");
 }
 
